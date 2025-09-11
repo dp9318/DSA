16e2f6b4011894de0e8fe86527d9ad38e594baf6
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-09-11 22:35:17 +0530

    commit_11_9_25_01

---
+++ Arrays/n_queries.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num_arrays; 
+    int queries;
+    for(int i=0; i<num_arrays; i++){
+        for(int j=0; j<queries; j++){
+            
+        }
+    }
+
+    return 0;
+}
