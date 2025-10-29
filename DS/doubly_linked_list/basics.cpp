e4c813a8e153fa6d73dc722332b096702b5d84ba
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-10-29 15:06:29 +0530

    Doubly linked list practice

---
+++ DS/doubly_linked_list/basics.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int ScholarID;
+    string Name;
+    Node *next;
+    Node *prev;
+};
+
+Node *head;
+Node *tail;
+
+void insertAtEnd(int id , string name){
+    Node *newnNode = new Node();
+    
+}
+
+int main(){
+
+    return 0;
+}
