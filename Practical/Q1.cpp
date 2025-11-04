ee417baa09d4c825c2eae93bfd4298e0b2227bbc
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-11-04 12:49:47 +0530

    commit_4_11_25_03

--- Practical/Q1.cpp
+++ Practical/Q1.cpp
@@ -2,35 +2,16 @@
 using namespace std;
 
 int main(){
-
     int n; 
-    cout<<"Enter the number of days: ";
     cin>>n;
     int arr[n];
-    cout<<"Enter the stock prices: ";
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
         cin>>arr[i];
-    }
-    int count=0;
-    int profit[n*n];
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            profit[count] = arr[j]-arr[i];
-            count++;
-        }
-    }
-    int max=profit[0];
-    for(int i=0; i<count; i++){
-        if(max<profit[i]){
-            max=profit[i];
-        }
-    }
-    
-    if(max<0){
-        max=0;
-    }
+    int max=0;
+    for(int i=0; i<n-1; i++)
+        for(int j=i+1; j<n; j++)
+            if(max<arr[j]-arr[i])
+                max=arr[j]-arr[i];
     cout<<max;
-
-
-    return 0;
+    return 0; 
 }
