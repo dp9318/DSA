9e20fb28439c205f8f669d70e27682dfd2a1ecc6
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-11-04 12:56:07 +0530

    commit_4_11_25_04

--- Practical/Q1.cpp
+++ Practical/Q1.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-    int n; 
+    int n,max=0;
     cin>>n;
     int arr[n];
     for(int i=0; i<n; i++)
         cin>>arr[i];
-    int max=0;
     for(int i=0; i<n-1; i++)
         for(int j=i+1; j<n; j++)
             if(max<arr[j]-arr[i])
