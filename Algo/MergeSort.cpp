d29f0bfa4e974702bdfcafcb70714a9a7d2278d7
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-11-20 00:46:31 +0530

    commit_20_11_25_01

--- Algo/MergeSort.cpp
+++ Algo/MergeSort.cpp
@@ -7,6 +7,8 @@ int *merge(int arr1[],int size1, int arr2[], int size2);
 
 int * input(int size);
 
+void print(int arr[], int size);
+
 int main(){
 
     int n; 
@@ -17,7 +19,8 @@ int main(){
     arr = input(n);
 
     cout<<"Sorted array: ";
-
+    arr=mergeSort(arr,n);
+    print(arr,n);
     return 0;
 }
 
@@ -27,6 +30,12 @@ int * input(int size){
         cin>>arr[i];
     }
     return arr;
+}
+
+void print(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"  ";
+    }
 }
 
 int *merge(int arr1[],int size1, int arr2[], int size2){
@@ -54,5 +63,21 @@ int *merge(int arr1[],int size1, int arr2[], int size2){
 }
 
 int* mergeSort(int arr[], int size){
-    
+    if(size==0 || size==1){
+        return arr;
+    }
+    int mid = size/2;
+
+    int *left = new int[mid];
+    for(int i=0; i<mid; i++){ 
+        left[i]=arr[i];
+    }
+    int *right = new int[size-mid];
+    for(int i=0; i<size-mid; i++){
+        right[i]=arr[mid+i];
+    }
+    int *leftSort = mergeSort(left,mid);
+    int *rightSort = mergeSort(right,size-mid);
+    int *result = merge(leftSort,mid,rightSort,size-mid);
+    return result;
 }
