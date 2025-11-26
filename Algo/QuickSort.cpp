73121ed189ca5aab93d4ca22c4fc45810aa1f1aa
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-11-26 21:10:59 +0530

    commit_26_11_02_completed_quickSort

--- Algo/QuickSort.cpp
+++ Algo/QuickSort.cpp
@@ -5,22 +5,48 @@ int partition(int *arr, int start, int end){
     int i=start-1;
     int pivot = arr[end];
     for(int j=start; j<end; j++){
-        
+        if(arr[j]<=pivot){
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
     }
-    return pivot;
+    int temp = arr[i+1];
+    arr[i+1] = arr[end];
+    arr[end] = temp;
+
+    return i+1;
 }
 
-int QuickSort(int *arr, int l, int r){
-
+void  QuickSort(int *arr, int l, int r){
     if(l<r){
-        int pivot = partition(arr,l,r);
-        QuickSort(arr,l,pivot-1);
-        QuickSort(arr, pivot+1, r);
+        int p = partition(arr,l,r);
+        QuickSort(arr,l,p-1);
+        QuickSort(arr, p+1, r);
     }
+}
 
+int *input(int *arr, int size){
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+    return arr;
 }
 
 int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int *arr = new int[n];
+    cout<<"Enter the elements: ";
+    arr = input(arr,n);
 
+    QuickSort(arr,0,n-1);
+
+    cout<<"\nSorted array: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
